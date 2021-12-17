d569d7ab3f559cf161fa3443798e4fd05da7effb
bumcheol1 <bumcheol1@gmail.com>
2021-12-17 20:50:40 +0900

    1

--- Baekjoon/MinMax.cpp
+++ Baekjoon/MinMax.cpp
@@ -4,7 +4,17 @@ using namespace std;
 
 int main(void)
 {
+    ios::sync_with_stdio(false); // cout, cin 속도 증가
     int N, i;
 
-
+    int array[1000000] = {};
+    
+    cin >> N;
+    
+    for(i = 0; i < N; i++){
+        cin >> array[i];
+    }
+    sort(array, array+N);
+    cout << array[0] << " " << array[N-1] << endl;
 }
+
