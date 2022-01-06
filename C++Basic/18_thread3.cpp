e5fcfb9d04825fe133523122285cb5ada903ae7b
bumcheol1 <bumcheol1@gmail.com>
2022-01-06 11:35:01 +0900

    220106

--- C++Basic/18_thread3.cpp
+++ C++Basic/18_thread3.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <thread>
 
+#if 0
 void foo(){
     std::cout << "foo" << std::endl;
 }
@@ -11,4 +12,17 @@ int main(){
     std::thread t1(&foo);
 
     t1.join();
+}
+#endif
+
+void thread_routine(const char* name){
+    std::cout << name << std::endl;
+}
+
+int main(){
+    std::thread t1(&thread_routine, "A");
+    std::thread t2(&thread_routine, "B");
+
+    t1.join();
+    t2.join();
 }
