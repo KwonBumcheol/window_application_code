d8e2b3010a46ba11ccb5b97bed073ec7227fcac4
POIUY <bumcheol1@gmail.com>
2022-01-07 22:46:05 +0900

    220107

--- Baekjoon/2920.cpp
+++ Baekjoon/2920.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main(){
-    vector<int> v;
+    ios::sync_with_stdio(0);
+    cout.tie(0);
+    cin.tie(0); // 입출력 속도 향상
+
+    vector<int> v(8);
+    vector<int> a(8);
+    vector<int> d(8);
 
     for(int i=0; i<8; i++){
         cin >> v[i];
+        a = v;
+        d = v;
+    }
+
+    sort(a.begin(), a.end());
+    sort(d.begin(), d.end(), greater<int>()); // greater<자료형>() - 내림차순 <-> less<자료형>() 내림차순
+
+    if(v == a){
+        cout << "ascending";
+    } else if(v == d){
+        cout << "descending";
+    } else{
+        cout << "mixed";
     }
 }
