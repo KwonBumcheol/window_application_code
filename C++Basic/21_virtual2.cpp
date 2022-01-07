75543a602911adecdd505a1e35ef16f8439872a2
POIUY <bumcheol1@gmail.com>
2022-01-07 10:24:23 +0900

    220106

--- C++Basic/21_virtual2.cpp
+++ C++Basic/21_virtual2.cpp
@@ -17,10 +17,16 @@ int main(){
     Animal* p1 = new Dog;
 
     Dog d;
-    Animal* p2 = &d;
+    Cat c;
+    // Animal* p2 = &d;
+    Animal* p2 = &c;
     
     // 부모의 포인터 타입을 자식의 포인터 타입으로의 암묵적 변환은 허용되지
     // 않는다.
     // => 명시적인 캐스팅이 필요하다.
     Dog* pDog = static_cast<Dog*>(p1);
+    cout << pDog << endl;
+    // static_cast: 다운 캐스팅은 잘못된 자식 타입으로 캐스팅 되는 위험이 있다.
+
+    // - dynamic_cast<>
 }
